524011c77df26dc3b8cc79f223e16185bab4f9c2
Zehab-bid <zehabidha15@gmail.com>
2026-09-24 21:29:20 +0700

    TIPE DATA DASAR dan COLLECTION
    Added examples of basic data types, lists, sets, and maps in Kotlin.

--- Main.kt
+++ Main.kt
@@ -4,3 +4,27 @@ fun main() {
     val customers = 10
     println("There are $customers customers")
 }
+
+fun main() {
+    // Tipe Data Dasar
+    val d: Int
+    d = 3
+    val e: String = "hello"
+
+    // List
+    val readOnlyShapes = listOf("triangle", "square", "circle")
+    println(readOnlyShapes)
+    
+    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+    println(shapes)
+
+    // Set
+    val readOnlyFruit = setOf("apple", "banana", "cherry", "cherry")
+    val fruit: MutableSet<String> = mutableSetOf("apple", "banana", "cherry", "cherry")
+    println(readOnlyFruit)
+
+    // Map
+    val readOnlyJuiceMenu = mapOf("apple" to 100, "kiwi" to 190, "orange" to 100)
+    println(readOnlyJuiceMenu)
+    println("The value of apple juice is: ${readOnlyJuiceMenu["apple"]}")
+}
